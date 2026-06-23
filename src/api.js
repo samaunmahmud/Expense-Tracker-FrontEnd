273f3c9299a8d08e13d1e16e2372db0fb3932120
samaunmahmud <samaunmahmud9@gmail.com>
2026-06-23 21:27:19 +0600

    Add axios API client with JWT interceptor. Centralized API client pointing to Spring Boot backend at localHost. Authomatically attaches Bearer token from localStorage to every outgoing request.

---
+++ src/api.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:8080/api";
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+// Attach the JWT to every outgoing request automatically, if we have one
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export const authApi = {
+  signup: (data) => api.post("/auth/signup", data),
+  login: (data) => api.post("/auth/login", data),
+};
+
+export const plaidApi = {
+  createLinkToken: () => api.post("/plaid/link-token"),
+  exchangeToken: (publicToken) =>
+    api.post("/plaid/exchange-token", { public_token: publicToken }),
+};
+
+export default api;
